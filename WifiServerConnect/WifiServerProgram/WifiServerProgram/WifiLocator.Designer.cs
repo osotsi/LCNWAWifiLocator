ca10359ff342377634f602ce6e078ef31506a029
osotsi <osotsi_ak@students.lynchburg.edu>
2014-03-04 14:36:11 -0500

    Wifi Server Project

---
+++ WifiServerConnect/WifiServerProgram/WifiServerProgram/WifiLocator.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WifiServerProgram
+{
+    partial class WifiLocator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userIdText = new System.Windows.Forms.TextBox();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.macAddressText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.sendToServerButton = new System.Windows.Forms.Button();
+            this.sendReport = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userIdText
+            // 
+            this.userIdText.Location = new System.Drawing.Point(32, 24);
+            this.userIdText.Name = "userIdText";
+            this.userIdText.Size = new System.Drawing.Size(143, 20);
+            this.userIdText.TabIndex = 1;
+            this.userIdText.Text = "Enter User Id...";
+            this.userIdText.Click += new System.EventHandler(this.userIdText_Click);
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(32, 135);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(143, 20);
+            this.firstNameText.TabIndex = 2;
+            this.firstNameText.Text = "Enter User First Name...";
+            this.firstNameText.Click += new System.EventHandler(this.firstNameText_Click);
+            // 
+            // emailText
+            // 
+            this.emailText.Location = new System.Drawing.Point(32, 206);
+            this.emailText.Name = "emailText";
+            this.emailText.Size = new System.Drawing.Size(143, 20);
+            this.emailText.TabIndex = 4;
+            this.emailText.Text = "Enter User Email...";
+            this.emailText.Click += new System.EventHandler(this.emailText_Click);
+            // 
+            // macAddressText
+            // 
+            this.macAddressText.Location = new System.Drawing.Point(32, 97);
+            this.macAddressText.Name = "macAddressText";
+            this.macAddressText.Size = new System.Drawing.Size(143, 20);
+            this.macAddressText.TabIndex = 3;
+            this.macAddressText.Text = "Enter User Mac Address...";
+            this.macAddressText.Click += new System.EventHandler(this.macAddressText_Click);
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(32, 171);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(143, 20);
+            this.lastNameText.TabIndex = 6;
+            this.lastNameText.Text = "Enter User Last Name...";
+            this.lastNameText.Click += new System.EventHandler(this.lastNameText_Click);
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(32, 62);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(143, 20);
+            this.passwordText.TabIndex = 5;
+            this.passwordText.Text = "Enter User Password...";
+            this.passwordText.Click += new System.EventHandler(this.passwordText_Click);
+            // 
+            // sendToServerButton
+            // 
+            this.sendToServerButton.Location = new System.Drawing.Point(424, 24);
+            this.sendToServerButton.Name = "sendToServerButton";
+            this.sendToServerButton.Size = new System.Drawing.Size(164, 58);
+            this.sendToServerButton.TabIndex = 7;
+            this.sendToServerButton.Text = "Send Information";
+            this.sendToServerButton.UseVisualStyleBackColor = true;
+            this.sendToServerButton.Click += new System.EventHandler(this.sendToServerButton_Click);
+            // 
+            // sendReport
+            // 
+            this.sendReport.AutoSize = true;
+            this.sendReport.Location = new System.Drawing.Point(29, 254);
+            this.sendReport.Name = "sendReport";
+            this.sendReport.Size = new System.Drawing.Size(244, 13);
+            this.sendReport.TabIndex = 8;
+            this.sendReport.Text = "Click Send To Send Information To The Database";
+            // 
+            // WifiLocator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sendReport);
+            this.Controls.Add(this.sendToServerButton);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.emailText);
+            this.Controls.Add(this.macAddressText);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.userIdText);
+            this.Name = "WifiLocator";
+            this.Text = "Wifi Locator Control Panel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userIdText;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.TextBox emailText;
+        private System.Windows.Forms.TextBox macAddressText;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Button sendToServerButton;
+        private System.Windows.Forms.Label sendReport;
+
+    }
+}
+
---
+++ WifiServerConnect/WifiServerProgram/WifiServerProgram/WifiLocator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+//Add MySql Library
+using MySql.Data.MySqlClient;
+
+namespace WifiServerProgram
+{
+    public partial class WifiLocator : Form
+    {
+
+
+        public WifiLocator()
+        {
+            InitializeComponent();
+        }
+
+        private void sendToServerButton_Click(object sender, EventArgs e)
+        {
+            if (userIdText.Text == "" || firstNameText.Text == "" || emailText.Text == "" || macAddressText.Text == "" || lastNameText.Text == "" || passwordText.Text == "")
+            {
+                sendReport.Text = "Fill in all the required information!";
+            }
+            else if (userIdText.Text.Contains("Enter ") || firstNameText.Text.Contains("Enter ") || emailText.Text.Contains("Enter ") || macAddressText.Text.Contains("Enter ") || lastNameText.Text.Contains("Enter ") || passwordText.Text.Contains("Enter "))
+            {
+                sendReport.Text = "Actually enter in some information...";
+            }
+            else
+            {
+                try
+                {
+                    String command = "INSERT INTO wifitable (UserID,password,usermacaddress,firstname,lastname,email,isadmin) VALUES('" +
+                    userIdText.Text + "','" + passwordText.Text + "','" + macAddressText.Text + "','" + firstNameText.Text + "','" + lastNameText.Text + "','" + emailText.Text + "','NO')";
+                    sendReport.Text = "Command Sent: " + command;
+
+                    MySqlConnection connection;
+                    string server;
+                    string database;
+                    string uid;
+                    string password;
+
+                    server = "mintaka.lynchburg.edu";
+                    database = "WifiLoc Dja";
+                    uid = "";
+                    password = "";
+                    string connectionString;
+                    connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                    database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+                    connection = new MySqlConnection(connectionString);
+
+                    //open connection
+                    //create command and assign the query and connection from the constructor
+                    MySqlCommand cmd = new MySqlCommand(command, connection);
+
+                    //Execute command
+                    cmd.ExecuteNonQuery();
+
+             
+                    
+                }
+                catch (Exception badError)
+                {
+                    Console.WriteLine("Error..... " + badError.StackTrace);
+                }
+            }
+        }
+
+        private void userIdText_Click(object sender, EventArgs e)
+        {
+            userIdText.Text = "";
+        }
+
+        private void passwordText_Click(object sender, EventArgs e)
+        {
+            passwordText.Text = "";
+        }
+
+        private void macAddressText_Click(object sender, EventArgs e)
+        {
+            macAddressText.Text = "";
+        }
+
+        private void firstNameText_Click(object sender, EventArgs e)
+        {
+            firstNameText.Text = "";
+        }
+
+        private void lastNameText_Click(object sender, EventArgs e)
+        {
+            lastNameText.Text = "";
+        }
+
+        private void emailText_Click(object sender, EventArgs e)
+        {
+            emailText.Text = "";
+        }
+
+    }
+}
